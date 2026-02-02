ccceded8c6d10614b955e960e80a72ccc5c875f7
Chakrit <chakrit@origami.life>
2026-02-03 01:38:29 +0700

    .

--- production.config.php
+++ production.config.php
@@ -5,3 +5,6 @@
 	define('DB_NAME', 'if0_40752821_windproject');
 	define('BASE_URL', 'https://winddemo.infinityfree.me');
 	define('APP_URL',  'https://winddemo.infinityfree.me');
+	define('key', 'yJj3mH8kF0cT1oZfTqPqvFj4T7t5Q3vYlT3W8yD2zZk=');
+    define('windy_key', 'd9f3MF1fFwG9k3A70totUNl2NrghgzXE');
+    define('GOOGLE_API_KEY', 'AIzaSyByNwzdqfq-8GMdWkUfANAkYL1sPsBhOgE');
