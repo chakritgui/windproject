9339ddbe4525ca32c2a9b25d7c035d90aca494d4
Chakrit <chakrit@origami.life>
2026-02-04 10:58:41 +0700

    .

--- config.sample.php
+++ config.sample.php
@@ -1,11 +1,11 @@
 <?php
-    define('DB_HOST', '127.0.0.1');
+    define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', '1234');
+    define('DB_PASS', '');
     define('DB_NAME', 'windproject');
     define('BASE_URL', 'http://localhost:8080/windproject');
     define('APP_URL', 'http://localhost:8080');
     define('key', 'yJj3mH8kF0cT1oZfTqPqvFj4T7t5Q3vYlT3W8yD2zZk=');
-    define('WINDY_KEY', 'd9f3MF1fFwG9k3A70totUNl2NrghgzXE');
-    define('GOOGLE_API_KEY', 'AIzaSyByNwzdqfq-8GMdWkUfANAkYL1sPsBhOgE');
+    define('WINDY_KEY', 'YOUR_WINDY_KEY');
+    define('GOOGLE_API_KEY', 'YOUR_GOOGLE_API_KEY');
     define('TRANSLATE_LIMIT', 450000);
