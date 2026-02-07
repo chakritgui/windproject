1e7e0c1169cfeb0fdd0c48930302ad0dd0e9f314
Chakrit <chakrit@origami.life>
2026-02-08 01:32:19 +0700

    .

--- sql/import.sql
+++ sql/import.sql
@@ -1,16 +1,18 @@
-INSERT INTO `system_settings` (`setting_key`, `setting_value`, `setting_group`, `description`) VALUES
+INSERT INTO `system_settings`
+(`setting_key`, `setting_value`, `setting_group`, `description`) VALUES
 ('WINDY_KEY', '', 'API', 'API Key สำหรับบริการ Windy.com'),
 ('GOOGLE_API_KEY', '', 'API', 'Google Maps/Services API Key'),
 ('MAIL_HOST', 'smtp.gmail.com', 'MAIL', 'Outgoing Mail Server (SMTP)'),
 ('MAIL_USER', '', 'MAIL', 'Email Account สำหรับส่งจดหมาย'),
 ('MAIL_PASS', '', 'MAIL', 'Password สำหรับ Email Account'),
 ('MAIL_PORT', '587', 'MAIL', 'Port สำหรับส่งเมล (เช่น 587, 465)'),
-('DOMAIN_NAME', 'http://localhost', 'GENERAL', 'URL หลักของเว็บไซต์');
-('ENABLE_TRANSLATE', '0', 'GENERAL', 'ใช้งานระบบแปลภาษา (Google Translate)');
-('NOTIFY_EMAIL', '1', 'GENERAL', 'การแจ้งเตือนผ่านอีเมล');
+('DOMAIN_NAME', 'http://147.50.254.79', 'GENERAL', 'URL หลักของเว็บไซต์'),
+('ENABLE_TRANSLATE', '0', 'GENERAL', 'ใช้งานระบบแปลภาษา (Google Translate)'),
+('NOTIFY_EMAIL', '1', 'GENERAL', 'การแจ้งเตือนผ่านอีเมล'),
 ('NOTIFY_PWA', '1', 'GENERAL', 'การแจ้งเตือนผ่าน PWA Push');
 
-INSERT INTO `wp_setting` (`setting_type`, `setting_value`, `created_at`, `updated_at`) VALUES
+INSERT INTO `wp_setting`
+(`setting_type`, `setting_value`, `created_at`, `updated_at`) VALUES
 ('logo', NULL, NOW(), NOW()),
 ('icon', NULL, NOW(), NOW()),
 ('website_en', NULL, NOW(), NOW()),
@@ -25,7 +27,9 @@ INSERT INTO `wp_setting` (`setting_type`, `setting_value`, `created_at`, `update
 ('language_content', NULL, NOW(), NOW()),
 ('infography', NULL, NOW(), NOW());
 
-INSERT INTO `wp_folder` (`id`, `name`, `code`, `type`, `level`, `parent_id`, `ref_id`, `content_id`, `notification_status`, `status`, `created_at`, `updated_at`) VALUES
+INSERT INTO `wp_folder`
+(`id`, `name`, `code`, `type`, `level`, `parent_id`, `ref_id`, `content_id`,
+ `notification_status`, `status`, `created_at`, `updated_at`) VALUES
 (1, 'PROJECT', NULL, 'root', 1, NULL, NULL, NULL, 'no', 'active', NOW(), NOW()),
 (2, 'MEASUREMENT STATION', NULL, 'root', 1, NULL, NULL, NULL, 'no', 'active', NOW(), NOW()),
 (3, 'EIA / EHIA', NULL, 'root', 1, NULL, NULL, NULL, 'no', 'active', NOW(), NOW()),
@@ -38,5 +42,24 @@ INSERT INTO `wp_folder` (`id`, `name`, `code`, `type`, `level`, `parent_id`, `re
 (10, 'TYPE', 'type', 'root', 4, 9, NULL, NULL, 'no', 'active', NOW(), NOW()),
 (11, 'INSTALLATION', 'installation', 'root', 5, 10, NULL, NULL, 'no', 'active', NOW(), NOW());
 
-INSERT INTO `wp_members`(`member_id`, `username`, `password_hash`, `first_name`, `last_name`, `email`, `phone`, `role`, `status`, `last_login_at`, `created_at`, `updated_at`, `remember_selector`, `remember_validator_hash`, `remember_expires_at`) VALUES 
-(1,'admin@windproject',md5('Wp2026')','Admin','System','admin@windproject.wp',null,'admin@windproject.wp','active','null,NOW(),NOW(),null,null,null)
+INSERT INTO `wp_members`
+(`member_id`, `username`, `password_hash`, `first_name`, `last_name`,
+ `email`, `phone`, `role`, `status`, `last_login_at`,
+ `created_at`, `updated_at`,
+ `remember_selector`, `remember_validator_hash`, `remember_expires_at`)
+VALUES
+(1,
+ 'admin@windproject',
+ MD5('Wp2026'),
+ 'Admin',
+ 'System',
+ 'admin@windproject.wp',
+ NULL,
+ 'admin',
+ 'active',
+ NULL,
+ NOW(),
+ NOW(),
+ NULL,
+ NULL,
+ NULL);
