1cde6664e5df3b3e41cbaf936d8b455cd50a4b1f
Chakrit <chakrit@origami.life>
2026-02-08 18:52:32 +0700

    .

--- app/views/admin/wind.php
+++ app/views/admin/wind.php
@@ -30,19 +30,19 @@
                     <p><i class="fa-solid fa-folder-tree"></i> <span data-i18n="project"></span></p>
                     <select id="filter_project" class="form-select filter"></select>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-2 col-6">
                     <p><i class="fa-solid fa-tower-broadcast"></i> <span data-i18n="pole"></span></p>
                     <select id="filter_pole" class="form-select filter"></select>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-2 col-6">
                     <p><i class="fa-solid fa-tags"></i> <span data-i18n="type"></span></p>
                     <select id="filter_type" class="form-select filter"></select>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-2 col-6">
                     <p><i class="fa-solid fa-location-dot"></i> <span data-i18n="installation"></span></p>
                     <select id="filter_installation" class="form-select filter"></select>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-2 col-6">
                     <p><i class="fa-solid fa-text-height"></i> <span data-i18n="height"></span></p>
                     <select id="filter_height" class="form-select filter"></select>
                 </div>
