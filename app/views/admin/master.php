b0c2d33c1d7ccaa580646155cd603b4eac093d96
Chakrit <chakrit@origami.life>
2026-02-08 18:51:02 +0700

    .

--- app/views/admin/master.php
+++ app/views/admin/master.php
@@ -138,11 +138,11 @@
                             <p><i class="fa-solid fa-folder-tree"></i> <span data-i18n="projects"></span></p>
                             <select id="filter_installation_project" class="form-select filter"></select>
                         </div>
-                        <div class="col-sm-2">
+                        <div class="col-sm-2 col-6">
                             <p><i class="fa-solid fa-tags"></i> <span data-i18n="type"></span></p>
                             <select id="filter_installation_type" class="form-select filter"></select>
                         </div>
-                        <div class="col-sm-2">
+                        <div class="col-sm-2 col-6">
                             <p><i class="fa-solid fa-circle-dot"></i> <span data-i18n="status"></span></p>
                             <select id="filter_installation_status" class="form-select filter"></select>
                         </div>
@@ -169,19 +169,19 @@
             <div class="card shadow-sm mb-4">
                 <div class="card-body">
                     <div class="row g-2 mb-3">
-                        <div class="col-sm-2">
+                        <div class="col-sm-2 col-6">
                             <p><i class="fa-solid fa-folder-tree"></i> <span data-i18n="projects"></span></p>
                             <select id="filter_pole_project" class="form-select filter"></select>
                         </div>
-                        <div class="col-sm-2">
+                        <div class="col-sm-2 col-6">
                             <p><i class="fa-solid fa-tags"></i> <span data-i18n="type"></span></p>
                             <select id="filter_pole_type" class="form-select filter"></select>
                         </div>
-                        <div class="col-sm-2">
+                        <div class="col-sm-2 col-6">
                             <p><i class="fa-solid fa-location-dot"></i> <span data-i18n="installation"></span></p>
                             <select id="filter_pole_installation" class="form-select filter"></select>
                         </div>
-                        <div class="col-sm-2">
+                        <div class="col-sm-2 col-6">
                             <p><i class="fa-solid fa-circle-dot"></i> <span data-i18n="status"></span></p>
                             <select id="filter_pole_status" class="form-select filter"></select>
                         </div>
