d55a758532177a52e28070748654b971ab7965df
Chakrit <chakrit@origami.life>
2026-02-11 19:13:14 +0700

    .

--- public/js/user/map.js
+++ public/js/user/map.js
@@ -15,7 +15,7 @@ function initMap() {
         map = api.map;
         const { store, picker } = api;
         poleLayerGroup = L.layerGroup().addTo(map);
-        store.set('overlay', 'fire');
+        store.set('overlay', 'fires');
         store.set('level', DEFAULT_LEVEL);
         try {
             const results = await Promise.allSettled([
