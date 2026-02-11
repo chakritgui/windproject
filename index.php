d56cc2f37b7f77311ed17ee721270fa117e2979b
Chakrit <chakrit@origami.life>
2026-02-11 13:28:12 +0700

    .

--- index.php
+++ index.php
@@ -53,8 +53,7 @@
     $router->post('/api/auth/forgot', 'AuthController@sendReset');
     $router->post('/api/auth/update-password', 'AuthController@updatePassword');
     // --- [B] จัดการหน้าหลัก (/) จุดเดียวจบ ---
-    $router->get('/', function() {
-        print_r($_SESSION); exit;
+    $router->get('', function() {
         if (!isset($_SESSION['user'])) {
             return (new AuthController())->login();
         }
