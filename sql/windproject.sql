7d6847580b99e1f05967c58483e9d25202ab0567
Chakrit <chakrit@origami.life>
2026-02-21 02:58:39 +0700

    .

--- sql/windproject.sql
+++ sql/windproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2026 at 06:34 PM
+-- Generation Time: Feb 20, 2026 at 08:58 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 8.0.30
 
@@ -294,9 +294,9 @@ CREATE TABLE `wp_folder` (
   `name` varchar(255) DEFAULT NULL,
   `slug` varchar(255) NOT NULL,
   `type` enum('folder','content') NOT NULL DEFAULT 'folder',
+  `sub_type` enum('news','project') NOT NULL DEFAULT 'project',
   `level` bigint(20) DEFAULT NULL,
   `parent_id` bigint(20) DEFAULT NULL,
-  `ref_id` bigint(20) DEFAULT NULL,
   `content_id` bigint(20) DEFAULT NULL,
   `status` enum('active','inactive','deleted') NOT NULL DEFAULT 'active',
   `created_at` datetime NOT NULL,
