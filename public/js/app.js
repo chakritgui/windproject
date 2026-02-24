5f142995cb2f159263ab51feed0d967865ce79a0
Chakrit <chakrit@origami.life>
2026-02-25 00:27:28 +0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -93,7 +93,6 @@ async function requestAndSubscribe(registration) {
             } catch (e) { console.warn("Server unsubscription silent fail", e); }
         }
         await registration.update();
-
         const permission = await Notification.requestPermission();
         if (permission !== 'granted') {
             throw new Error(langData['permission_denied'] || "Permission not granted");
