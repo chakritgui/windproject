c5f72b8180cbb40c2df0baeb5ffb94937f8a4e86
Chakrit <chakrit@origami.life>
2026-02-25 12:38:16 +0700

    .

--- app/controllers/PushController.php
+++ app/controllers/PushController.php
@@ -22,9 +22,6 @@ class  PushController extends BaseController {
         }
     }
     public function unsubscribe() {
-                 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
         print_r($data);
