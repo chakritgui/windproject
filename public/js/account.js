0c119990f0e42f762d81554acbc72398c0b2b00b
Chakrit <chakrit@origami.life>
2026-03-23 23:52:13 +0700

    .

--- public/js/account.js
+++ public/js/account.js
@@ -347,7 +347,7 @@ function loadUsageHistory() {
                                                     ${log.ip_address}
                                                 </small>
                                                 ${log.login_location ? `
-                                                    <span class="location-link text-blue small" style="font-size: 0.6rem;">
+                                                    <span class="text-blue small" style="font-size: 0.6rem;">
                                                         <i class="fa-solid fa-location-dot me-1"></i>${log.login_location}
                                                     </span>
                                                 ` : ''}
