9d049496da6b13a61c0670c199037fc2374dc114
Chakrit <chakrit@origami.life>
2026-03-26 01:44:16 +0700

    .

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     }
     ini_set('session.cookie_samesite', 'Lax');
     session_start();
-    print_r($_SESSION['user']['allowedPaths']); exit;
     require_once __DIR__ . '/vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
